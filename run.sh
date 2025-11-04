3a397c20f0e24e3e4cbfcaaa5b243210ecc66e50
Alex Haydock <alex@alexhaydock.co.uk>
2025-11-04 11:31:18 +0000

    Updates

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 command -v ansible-playbook >/dev/null || sudo dnf install -y ansible ansible-collection-community-general sshpass
+command -v pre-commit >/dev/null || sudo dnf install -y pre-commit
+pre-commit install --hook-type pre-push
 ansible-playbook playbook.yml -i inventory.ini --ask-pass --ask-become-pass
